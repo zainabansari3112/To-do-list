a28b69dc6dbc424332a5b38ce244336d78ade10c
zainabansari3112 <mask5414@gmail.com>
2025-07-28 11:38:29 +0530

    Deploy-ready changes

--- server/db.js
+++ server/db.js
@@ -1,11 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: "postgres",
-  password: "123",
-  host: "localhost",
-  port: 5432,
-  database: "perntodo"
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
 });
-export default pool; 
+
+export default pool;
